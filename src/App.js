8c70a05a515c32da0fd729efa50e1a57a436fe79
Juan Carlos <jcrpelaez@yahoo.com>
2020-02-25 15:05:19 -0600

    Added router and security

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
@@ -10,13 +11,29 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/parents" exact={true} component={Parent} />
-        </div> 
-      </div>
+      <Security
+        issuer="https://dev-694554.okta.com/oauth2/default"
+        clientId="0oa2lhyd99BR4KYDo4x6"
+        redirectUri={window.location.origin + "/implicit/callback"}
+        onAuthRequired={onAuthRequired}
+        pkce={true}
+      >
+        <Route path="/" exact={true} component={Home} />
+        <SecureRoute path="/protected" component={Protected} />
+        <Route
+          path="/login"
+          render={() => <Login baseUrl="https://${yourOktaDomain}" />}
+        />
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/parents" exact={true} component={Parent} />
+          </div>
+        </div>
+      </Security>
     </Router>
   );
 }
